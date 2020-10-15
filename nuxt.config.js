cff7214b8039c58e6777257432cbab6cfbb2686d
Sam Littlefair <sam.littlefair@prismic.io>
2020-10-15 12:16:51 +0200

    Move apiEndpoint to sm.json, import to nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,10 +9,6 @@ export default {
   head: {
     title: "Nuxt + Prismic",
     meta: [
-<<<<<<< HEAD
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-=======
       {
         charset: "utf-8"
       },
@@ -20,7 +16,6 @@ export default {
         name: "viewport",
         content: "width=device-width, initial-scale=1"
       },
->>>>>>> master
       {
         hid: "description",
         name: "description",
@@ -28,15 +23,11 @@ export default {
       }
     ],
     link: [
-<<<<<<< HEAD
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
-=======
       {
         rel: "icon",
         type: "image/x-icon",
         href: "/favicon.ico"
       },
->>>>>>> master
       {
         rel: "stylesheet",
         href:
@@ -51,8 +42,6 @@ export default {
         rel: "stylesheet",
         href: "https://fonts.googleapis.com/icon?family=Material+Icons"
       }
-<<<<<<< HEAD
-=======
     ],
     script: [
       {
@@ -63,59 +52,36 @@ export default {
         src:
           "https://cdn.jsdelivr.net/npm/focus-visible@5.0.2/dist/focus-visible.min.js"
       }
->>>>>>> master
     ]
   },
 
   /*
    ** Customize the progress-bar color
    */
-<<<<<<< HEAD
-  loading: { color: "#fff" },
-=======
   loading: {
     color: "#fff"
   },
->>>>>>> master
 
   /*
    ** Global CSS
    */
-<<<<<<< HEAD
-  css: ["@/assets/css/resetr.css", "@/assets/css/common.css"],
-=======
   css: [
     "vue-essential-slices/src/styles/styles.scss",
     "@/assets/css/resetr.css",
     "@/assets/css/common.css"
   ],
->>>>>>> master
 
   /*
    ** Plugins to load before mounting the App
    */
-<<<<<<< HEAD
-  plugins: [{ src: "~/plugins/prismicLinks", ssr: false }],
-=======
   plugins: [],
->>>>>>> master
 
   /*
    ** Nuxt.js modules
    */
-<<<<<<< HEAD
-  modules: [
-    // https://prismic-nuxt.js.org/
-    "@nuxtjs/prismic"
-  ],
-
-  prismic: {
-    endpoint: apiEndpoint,
-=======
   modules: [["@nuxtjs/prismic"], ["nuxt-sm"]],
   prismic: {
-    endpoint: "https://your-repo-name.cdn.prismic.io/api/v2",
->>>>>>> master
+    endpoint: apiEndpoint,
     linkResolver: "@/plugins/link-resolver",
     htmlSerializer: "@/plugins/html-serializer"
   },
@@ -130,12 +96,7 @@ export default {
     extend(config, ctx) {
       // to transform link with <nuxt-link> for the htmlSerializer
       config.resolve.alias["vue"] = "vue/dist/vue.common";
-<<<<<<< HEAD
-    }
-  },
-=======
     },
->>>>>>> master
 
     transpile: ["vue-slicezone", "nuxt-sm"]
   },
